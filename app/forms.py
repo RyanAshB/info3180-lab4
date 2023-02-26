04e68621c070f94a10110d935734a08d79ecf785
RyanAsh <ryanash.b@gmail.com>
2023-02-26 17:45:59 -0500

    lab4

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, FileField
 from wtforms.validators import InputRequired
+from flask_wtf.file import FileAllowed, FileRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class UploadForm(FlaskForm):
+    image = FileField('Image', validators=[InputRequired(message='Required'), FileAllowed(['jpg', 'png'])])
--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash
 from app.models import UserProfile
 from app.forms import LoginForm
+from app.forms import UploadForm
 
 
 ###
@@ -25,15 +26,38 @@ def about():
 
 
 @app.route('/upload', methods=['POST', 'GET'])
+@login_required
 def upload():
     # Instantiate your form class
+    form = UploadForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('File Saved', 'success')
+        return redirect(url_for('upload'))
+
+    return render_template('upload.html', form=form)
+
 
     # Validate file upload on submit
     if form.validate_on_submit():
         # Get file data and save to your uploads folder
+        username = form.username.data
+        password = form.password.data
 
-        flash('File Saved', 'success')
-        return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
+        user = UserProfile.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            # Log the user in and go to home page if the password is right
+            login_user(user)
+            flash('Logged in.', 'success')
+            return redirect(url_for('upload'))
+        else:
+            #do not log in
+            flash('Invalid username or password', 'danger')
 
     return render_template('upload.html')
 
@@ -67,12 +91,11 @@ def login():
 
             flash('Logged in successfully.', 'success')
 
-    
-        # Gets user id, load into session
-        login_user(user)
+            return redirect(url_for('upload'))  # The user should be redirected to the upload form instead
+        
+        flash('Invalid username or password', 'danger')
 
-        # Remember to flash a message to the user
-        return redirect(url_for('upload'))  # The user should be redirected to the upload form instead
+        
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
@@ -93,6 +116,9 @@ def flash_errors(form):
                 getattr(form, field).label.text,
                 error
 ), 'danger')
+            
+
+
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
@@ -116,3 +142,35 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+from flask import send_from_directory
+
+@app.route('/uploads/<filename>')
+def get_image(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+def get_uploaded_images():
+    """
+    Returns a list of filenames of all uploaded images in the uploads folder
+    """
+    rootdir = os.getcwd()
+    upload_dir = os.path.join(rootdir, 'uploads')
+    uploaded_images = []
+    for subdir, dirs, files in os.walk(upload_dir):
+        for file in files:
+            if file.endswith('.jpg') or file.endswith('.png') or file.endswith('.gif'):
+                uploaded_images.append(file)
+    return uploaded_images
+
+from flask import render_template, url_for
+import os
+
+@app.route('/files')
+@login_required
+def files():
+    images = get_uploaded_images()
+    image_urls = [url_for('get_image', filename=image) for image in images]
+    return render_template('files.html', image_urls=image_urls)
+
+from flask_login import logout_user
